9a210876c4208e0b0875cc3acf706f0d6b48edca
Rohan rana <rohanrana33042@gmail.com>
2025-06-04 22:35:13 +0530

    Learned YOYO functionality

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-gsap.from("#box1", { x: -60, y: -60, duration: 1, delay: 1, rotate: -10 });
+gsap.to("#box1", {
+  x: 900,
+  duration: 1,
+  delay: 1,
+  rotate: 360,
+  repeat: -1,
+  yoyo: true,
+});
 // gsap.to("#box1", { x: 1000, duration: 2, delay: 1 });
 
 // gsap.from("#box2", { x: 500, y: 100, duration: 2, delay: 1 });
-gsap.from("h1", {
-  y: 200,
-  delay: 1,
-  duration: 2,
-  opacity: 0,
-  color: "red",
-  stagger: 1,
-});
+// gsap.from("h1", {
+//   y: 200,
+//   delay: 1,
+//   duration: 2,
+//   opacity: 0,
+//   color: "red",
+//   stagger: 1,
+// });
