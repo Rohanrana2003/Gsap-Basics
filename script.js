cbec98a8f53cbaf173e97246fe081ede068ff75e
Rohan rana <rohanrana33042@gmail.com>
2025-06-04 20:43:08 +0530

    First commit again

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.from("#box1", { x: -60, y: -60, duration: 1, delay: 1, rotate: -10 });
+// gsap.to("#box1", { x: 1000, duration: 2, delay: 1 });
+
+// gsap.from("#box2", { x: 500, y: 100, duration: 2, delay: 1 });
