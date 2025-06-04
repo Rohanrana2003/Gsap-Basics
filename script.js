54b2307d1267e5d0dbf9ca94351a1671aaa99ad6
Rohan rana <rohanrana33042@gmail.com>
2025-06-04 22:55:04 +0530

    Understand Time line

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-gsap.to("#box1", {
-  x: 900,
-  duration: 1,
-  delay: 1,
-  rotate: 360,
-  repeat: -1,
-  yoyo: true,
-});
+// gsap.to("#box1", {
+//   x: 900,
+//   duration: 1,
+//   delay: 1,
+//   rotate: 360,
+//   repeat: -1,
+//   yoyo: true,
+// });
+
 // gsap.to("#box1", { x: 1000, duration: 2, delay: 1 });
 
 // gsap.from("#box2", { x: 500, y: 100, duration: 2, delay: 1 });
@@ -17,3 +18,18 @@ gsap.to("#box1", {
 //   color: "red",
 //   stagger: 1,
 // });
+
+var tl = gsap.timeline();
+
+tl.from("h1", {
+  opacity: 0,
+  y: -20,
+  duration: 0.5,
+  delay: 0.5,
+});
+tl.from("h4", {
+  opacity: 0,
+  y: -20,
+  duration: 1,
+  stagger: 0.2,
+});
